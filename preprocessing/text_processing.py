79ae43b0f6587ebd053322678b9b2cc28b50fc74
Chaitanya <chaitanya.kulkarni2334@gmail.com>
2017-06-26 23:34:28 -0400

    added support for replacing each digit in text with a '0'

--- preprocessing/text_processing.py
+++ preprocessing/text_processing.py
@@ -1,5 +1,5 @@
 # TODO figure out the right name for this python file
-
+import re
 from gensim.models import Word2Vec, KeyedVectors
 from keras.preprocessing.text import Tokenizer
 from torch import LongTensor, cuda
@@ -8,13 +8,43 @@ from corpus.Manager import Manager, Dataset
 import config as cfg
 import numpy as np
 import itertools
+import collections
 
 
-def prepare_embeddings(use_norm=False, load_bin=True):
+def gen_list2id_dict(list_, min_freq=-1, insert_words=None, lowercase=False, replace_digits=False):
+    """
+    Iterates over texts and creates a word2id mapping.
+    """
+    counter = collections.Counter()
+    for item in list_:
+        if lowercase == True:
+            item = item.lower()
+        if replace_digits == True:
+            item = re.sub(r'\d', '0', item)
+        counter.update(item.strip().split())
+
+    list2id = collections.OrderedDict()
+    if insert_words is not None:
+        for word in insert_words:
+            list2id[word] = len(list2id)
+
+    word_count_list = counter.most_common()
+
+    for (word, count) in word_count_list:
+        if min_freq <= 0 or count >= min_freq:
+            list2id[word] = len(list2id)
+
+    return list2id
+
+
+def prepare_embeddings(use_norm=False, replace_digit=True, load_bin=True, support_start_stop=True):
     corpus = Manager(corpus_path=cfg.CORPUS_FOLDERPATH, common_path=cfg.COMMON_FOLDERPATH)
 
     # get all the sentences each sentence is a sequence of words (list of words)
     sent_iter = corpus.load_tokenized_sents(corpus.load_textfiles())
+
+    if replace_digit:
+        sent_iter = corpus.replace_num(sent_iter)
 
     # train a skip gram model to generate word vectors. Vectors will be of dimension given by 'size' parameter.
     print("         Loading Word2Vec ...")
@@ -25,20 +55,38 @@ def prepare_embeddings(use_norm=False, load_bin=True):
         skip_gram_model = Word2Vec(sentences=sent_iter, size=cfg.EMBEDDING_DIM, sg=1, window=10, min_count=1, workers=4)
 
     texts = corpus.load_sents(corpus.load_textfiles())
+
     cfg.ver_print("word2vec emb size", skip_gram_model.vector_size)
     # TODO fix tokenizer. Incorrect values.
+
+    # this is fine. this is a hack. i took away the tokenizing part of this class. i use it just to create the word index.
     tokenizer = Tokenizer(char_level=True)
     sent_iter_flat = list(itertools.chain.from_iterable(sent_iter))
+
+    list_of_chars = list(itertools.chain.from_iterable([list(word) for word in sent_iter_flat]))
 
     tokenizer.fit_on_texts(sent_iter)
 
     word_index = tokenizer.word_index
+
+    char_index = gen_list2id_dict(list_of_chars, insert_words=['<w>', '</w>', '<s>', '</s>'])
+
+    print(char_index)
+
+    cfg.CHAR_VOCAB = len(char_index.items())
+
+    if support_start_stop:
+        word_index['<s>'] = len(word_index)
+        word_index['</s>'] = len(word_index)
 
     with open('test_tokenizer.txt', 'w', encoding='utf-8') as out:
         out.writelines([item + ' ' + str(word_index[item]) + '\n' for item in sent_iter_flat])
 
     embedding_matrix = np.zeros((len(word_index) + 1, cfg.EMBEDDING_DIM))
     print("         Populating Embedding Matrix ...")
+    with open(cfg.OOV_FILEPATH, 'w') as f:
+        f.write("Out of Vocabulary words\n")
+
     for word, i in word_index.items():
         try:
             embedding_vector = skip_gram_model[word]
@@ -46,11 +94,11 @@ def prepare_embeddings(use_norm=False, load_bin=True):
         except KeyError:
             # not found in vocab
             # words not found in embedding index will be all-zeros.
-            with open(cfg.OOV_FILEPATH, 'w') as f:
+            with open(cfg.OOV_FILEPATH, 'a') as f:
                 f.write('{0}\n'.format(word))
             cfg.ver_print('out of vocabulary word', word)
 
-    return embedding_matrix, word_index
+    return embedding_matrix, word_index, char_index
 
 
 # this will convert a batch of variable length samples into padded tensor.
