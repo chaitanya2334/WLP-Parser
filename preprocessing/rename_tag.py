fcd08ddd36e0b71ca542ed3b564ab3e1789194a7
Chaitanya <chaitanya.kulkarni2334@gmail.com>
2017-07-03 01:13:56 -0400

    wrote a temp function to quickly rename "Action-Verb" to "Action". Remove this later.

---
+++ preprocessing/rename_tag.py
@@ -0,0 +1,23 @@
+import glob
+import os
+
+from corpus.Manager import Manager
+import config as cfg
+import fileinput
+
+def rename_tag(fpaths, ext=".ann", replace_from="Action-Verb", replace_to="Action", times=None):
+    for fpath in fpaths:
+        p = os.path.dirname(fpath)
+        title, _ = os.path.splitext(os.path.basename(fpath))
+        full_path = os.path.join(p, title + ext)
+        print(full_path)
+        with fileinput.FileInput(full_path, inplace=True, backup='.bak') as file:
+            for line in file:
+                print(line.replace(replace_from, replace_to), end='')
+
+
+if __name__ == '__main__':
+    corpus = Manager()
+
+    files = glob.glob(cfg.ARTICLES_FOLDERPATH + "/*.ann")
+    rename_tag(files)
